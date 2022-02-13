54b0db50ca6723e2560a915b78c168fcb457f71d
David Palmer <david.palmer@adscale.co.nz>
2022-02-13 13:17:31 +1300

    add a token refresh endpoint

--- server.js
+++ server.js
@@ -61,6 +61,36 @@ function authenticate(code, cb) {
   req.on('error', function(e) { cb(e.message); });
 }
 
+function refresh(code, cb) {
+  var data = qs.stringify({
+    client_id: config.oauth_client_id,
+    client_secret: config.oauth_client_secret,
+    refresh_token: code,
+    grant_type: 'refresh_token'
+  });
+
+  var reqOptions = {
+    host: config.oauth_host,
+    port: config.oauth_port,
+    path: config.oauth_path,
+    method: config.oauth_method,
+    headers: { 'content-length': data.length }
+  };
+
+  var body = "";
+  var req = https.request(reqOptions, function(res) {
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) { body += chunk; });
+    res.on('end', function() {
+      cb(null, qs.parse(body).access_token);
+    });
+  });
+
+  req.write(data);
+  req.end();
+  req.on('error', function(e) { cb(e.message); });
+}
+
 /**
  * Handles logging to the console.
  * Logged values can be sanitized before they are logged
@@ -94,6 +124,21 @@ app.all('*', function (req, res, next) {
 
 app.get('/authenticate/:code', function(req, res) {
   log('authenticating code:', req.params.code, true);
+  authenticate(req.params.code, function(err, token) {
+    var result
+    if ( err || !token ) {
+      result = {"error": err || "bad_code"};
+      log(result.error);
+    } else {
+      result = {"token": token};
+      log("token", result.token, true);
+    }
+    res.json(result);
+  });
+});
+
+app.get('/refresh/:code', function(req, res) {
+  log('refreshing code:', req.params.code, true);
   authenticate(req.params.code, function(err, token) {
     var result
     if ( err || !token ) {
